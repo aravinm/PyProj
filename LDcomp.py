856195027f189dde21337285aca239458df93d0c
Gjhmnjhn <46929228+Gjhmnjhn@users.noreply.github.com>
2019-02-07 22:09:02 +0800

    Update LDcomp.py
    removed temp vars
    connected to main combined get likes/dislike function (can also sperate back int getlike and get dislike if prefered)
    
    compacted code for replacing spaces in list of likes
    
    broke up unpacking to multiple line to make it easier to read and edit

--- LDcomp.py
+++ LDcomp.py
@@ -1,39 +1,36 @@
-import os
-
-#below are temp to be transferred to main
-directory = 'C:/Users/jacob/Desktop/Sampledata/'
-male_profiles, female_profiles, similarLD = ({} for i in range(3))
-
-def getLikes(dict):
-    profiledict = dict
-    listoflikes = []
-    listoflikes = profiledict['Likes'].rstrip().split(',',)
-    for x, y in enumerate(listoflikes):
-        listoflikes[x] = y.replace(" ", "")
-    return listoflikes
-
-def getDislikes(dict):
-    profiledict = dict
-    listoflikes = []
-    listoflikes = profiledict['Dislikes'].rstrip().split(',',)
-    for x, y in enumerate(listoflikes):
-        listoflikes[x] = y.replace(" ", "")
-    return listoflikes
 
 #thinking on how to compare likes with dislikes
-for x in male_profiles:
-    femalescore = {}
-    for y in female_profiles:
-        maleprofile, femaleprofile = male_profiles[x], female_profiles[y]
-        mlike, mdislike, flike, fdislike = getLikes(maleprofile), getDislikes(maleprofile), getLikes(femaleprofile), getDislikes(femaleprofile)
-        similarlikes, similardislikes, likedislike, dislikelike, totalem = len(set(mlike) & set(flike)), len(set(mdislike) & set(fdislike)), len(set(mlike) & set(fdislike)), len(set(mdislike) & set(flike)), len(mlike) + len(mdislike)
-        totalsim = similarlikes + similardislikes
-        totaldif = likedislike + dislikelike
-        finalscore = totalsim - totaldif
-        percentage = 100 * totalsim / totalem
-        fname,mname = femaleprofile['Name'], maleprofile['Name']
-        femalescore[fname] = percentage
-        similarLD[mname] = femalescore
-        
-   
-print similarLD
+def best_match(male_profiles, female_profiles):
+    similarLD = {}
+    for x in male_profiles:
+        femalescore = {}
+        for y in female_profiles:
+            maleprofile, femaleprofile = male_profiles[x], female_profiles[y]
+            mlike, mdislike, flike, fdislike = \
+                getLikes(maleprofile,"Likes"), \
+                getLikes(maleprofile,"Dislikes"), \
+                getLikes(femaleprofile,"Likes"), \
+                getLikes(femaleprofile,"Dislikes")
+            similarlikes, similardislikes, likedislike, dislikelike, totalem = \
+                len(set(mlike) & set(flike)), \
+                len(set(mdislike) & set(fdislike)), \
+                len(set(mlike) & set(fdislike)), \
+                len(set(mdislike) & set(flike)), \
+                len(mlike) + len(mdislike)
+            totalsim = similarlikes + similardislikes
+            totaldif = likedislike + dislikelike
+            finalscore = totalsim - totaldif
+            percentage = 100 * totalsim / totalem
+            fname,mname = femaleprofile['Name'], maleprofile['Name']
+            femalescore[fname] = percentage
+            similarLD[mname] = femalescore
+    return similarLD
+
+def getLikes(dict,key):
+    profiledict = dict
+    listoflikes = []
+    listoflikes = profiledict[key].rstrip().split(',',)
+    listoflikes = [x.replace(" ","") for x in listoflikes]
+    return listoflikes
+
+
