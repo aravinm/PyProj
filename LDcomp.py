7375bdb7d62138f48a15a89a5a210c47f1e0beb0
Gjhmnjhn <46929228+Gjhmnjhn@users.noreply.github.com>
2019-02-11 23:10:34 +0800

    Update LDcomp.py
    Added docstrings, might update again in the future

--- LDcomp.py
+++ LDcomp.py
@@ -1,6 +1,15 @@
 import matching
 
+'''This module includes the functions to calculate the compability score between two profiles.'''
+
+
 def match(suitor, partner):
+    """This function compares the number of similar likes and dislikes with the number of different like/dislikes and dislike/likes.
+       The score is calculated by the sum of total similarities (similar likes and dislikes) minus the differences of each profile's
+       likes and dislikes divided by total elements * 100 to get a percentage. The percentage for both profiles is then divded by 
+       2 to get the average score. This function returns the score (rounded to 2 decimal place) of the compatability of the 2 profiles       
+    """
+        
     p_likes,s_likes,p_dislikes,s_dislikes = \
         partner['Likes'], \
         suitor['Likes'], \
