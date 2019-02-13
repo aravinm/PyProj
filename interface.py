7c28d989149412b7239496e040649b03c034ebf5
Gjhmnjhn <46929228+Gjhmnjhn@users.noreply.github.com>
2019-02-13 19:47:39 +0800

    added cur user setting

--- interface.py
+++ interface.py
@@ -20,9 +20,20 @@ class Interface:
         self.style.theme_use("clam")
 
         self.content = ttk.Frame(root)
-        self.content.grid(column=0, row=0)
+        self.content.grid()
 
-        self.notebook = ttk.Notebook(self.content, width=200)
+        self.notebook = ttk.Notebook(self.content)
+
+        self.cur_user_label = ttk.LabelFrame(
+            self.content,
+            text = 'Current user:'
+        )
+        self.cur_user_value = tk.Label(
+            self.cur_user_label,
+            textvariable=self.cur_user
+        )
+        self.cur_user_label.grid(column=0, row=0, sticky='w')
+        self.cur_user_value.grid(column=0, row=0)
 
         self.dir_page = ttk.Frame(self.notebook)
         self.cur_dir_label = ttk.LabelFrame(
@@ -43,7 +54,7 @@ class Interface:
         self.choose_path_btn.grid(column=1, row=0)
 
         self.profile_page = ttk.Frame(self.notebook)
-        self.li = tk.Listbox(self.profile_page)
+        self.li = tk.Listbox(self.profile_page, height=15)
         self.li.bind("<<ListboxSelect>>", self.update_displayed_profile)
         self.displayed_profile = tk.Label(
             self.profile_page,
@@ -59,39 +70,45 @@ class Interface:
             text = 'set current user',
             command = self.set_cur_user
             )
-        self.li.grid(column=0, row=0)
-        self.displayed_profile.grid(column=1, row=0)
         self.cur_user_btn.grid(column=0, row=1)
         self.show_all_profile_btn.grid(column=0, row=2)
+        self.disable_if_empty()
 
+        self.match_page = ttk.Frame(self.notebook)
 
-        self.notebook.grid(column=0, row=0)
+        self.notebook.grid(column=0, row=1)
         self.notebook.add(self.dir_page, text='folders')
         self.notebook.add(self.profile_page, text='profiles')
+        self.notebook.add(self.match_page, text='best match')
 
 
-    def get_cur_selected_value(self,listbox):
+    def get_cur_selected_value(self):
         with ignored(tk.TclError):
             return self.li.get(self.li.curselection())
 
 
-    @staticmethod
-    def disable_if_empty(listbox, *widgets):
-        if not listbox.get(0):
-            listbox.grid_forget()
-            for widget in widgets:
-                widget.configure(state='disabled')
+    def disable_if_empty(self):
+        if not self.li.get(0):
+            self.li.grid_forget()
+            self.show_all_profile_btn.configure(state='disabled')
+            self.cur_user_btn.configure(state='disabled')
         else:
-            listbox.grid()
+            self.show_all_profile_btn.configure(state='enabled')
+            self.cur_user_btn.configure(state='enabled')
+            self.li.grid(column=0, row=0)
 
 
     def update_displayed_profile(self, event):
         if self.li.get(0):
             cur_selected_profile = reader.read_txt(
-                name = self.get_cur_selected_value(self.li),
+                name = self.get_cur_selected_value(),
                 directory = self.cur_dir.get()
                 )
             self.displayed_profile_text.set(cur_selected_profile)
+            if self.displayed_profile_text.get():
+                self.displayed_profile.grid(column=1, row=0, sticky='n')
+            else:
+                self.displayed_profile.grid_forget()
 
 
     def show_all_profiles(self):
@@ -109,10 +126,10 @@ class Interface:
     def set_cur_dir(self):
         self.cur_dir.set(fd.askdirectory()+'/')
         self.update_file_list()
-
+        self.displayed_profile_text.set('')
 
     def set_cur_user(self):
-        self.cur_user.set(self.get_cur_selected_value(self.li))
+        self.cur_user.set(self.get_cur_selected_value())
 
     def update_file_list(self):
         self.li.delete(0,'end')
@@ -120,7 +137,7 @@ class Interface:
             for file_name in os.listdir(self.cur_dir.get()):
                 if  file_name.endswith('.txt'):
                     self.li.insert('end', file_name)
-        self.disable_if_empty(self.li, self.show_all_profile_btn)
+        self.disable_if_empty()
 
 
 rt = tk.Tk()
